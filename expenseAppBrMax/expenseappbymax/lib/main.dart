aa81154edee63506baa4a97a0d97d6759acfce9a
khsh90 <khsh400@gmail.com>
2022-04-16 18:36:30 +0300

    expense App by max just start

---
+++ expenseAppBrMax/expenseappbymax/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  //  MyApp(Key? key):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ExpenseApp(),
+    );
+  }
+}
+
+class ExpenseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Expense App'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: const Card(
+                child: Text('DashBoard'),
+                elevation: 5,
+                color: Colors.blue,
+              ),
+            ),
+            const Card(
+              child: Text('Expesne Data'),
+            )
+          ],
+        ));
+  }
+}
