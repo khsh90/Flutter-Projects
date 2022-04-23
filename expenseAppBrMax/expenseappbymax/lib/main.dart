bf74d2d39d29f8b024cb512761a74ffb182f7b5c
Khaled Hadidi <khsh400@gmail.com>
2022-04-23 12:28:22 +0300

    after add action and floating action button

--- expenseAppBrMax/expenseappbymax/lib/main.dart
+++ expenseAppBrMax/expenseappbymax/lib/main.dart
@@ -1,6 +1,5 @@
 import '../widgets/userTransaction.dart';
 
-import 'models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -20,23 +19,28 @@ class ExpenseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Expense App'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                child: const Card(
-                  child: Text('DashBoard'),
-                  elevation: 5,
-                  color: Colors.blue,
-                ),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        title: const Text('Expense App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: const Card(
+                child: Text('DashBoard'),
+                elevation: 5,
+                color: Colors.blue,
               ),
-              UserTransaction()
-            ],
-          ),
-        ));
+            ),
+            UserTransaction()
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+    );
   }
 }
