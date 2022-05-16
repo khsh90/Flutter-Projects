ef8b415355d1d8728ca6216a8d99df8f2d69c7d3
khsh90 <khsh400@gmail.com>
2022-05-16 08:51:19 +0300

    app adaptve and resposive solved by max ;

--- expenseAppBrMax/expenseappbymax/lib/widgets/adabtivebutton.dart
+++ expenseAppBrMax/expenseappbymax/lib/widgets/adabtivebutton.dart
@@ -2,30 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io';
 
-class AdaptiveButton extends StatelessWidget{
-
-  final String text ; 
+class AdaptiveButton extends StatelessWidget {
+  final String text;
   final VoidCallback handler;
 
-  AdaptiveButton(this.text,this.handler);
+  AdaptiveButton(this.text, this.handler);
 
-  @override 
-Widget build (BuildContext context){
-
-  return Platform.isIOS
-                  ? CupertinoButton(
-                      onPressed: handler,
-                      child:  Text(text),
-                    )
-                  : ElevatedButton(
-                      onPressed: handler,
-                      child:  Text(text),
-                      style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all(Colors.white)))
-
-  
-
-
-}
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            onPressed: handler,
+            child: Text(text),
+          )
+        : ElevatedButton(
+            onPressed: handler,
+            child: Text(text),
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.white)));
+  }
 }
