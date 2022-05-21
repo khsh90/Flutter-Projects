7155dac42f15349d4341db4afba0b5c2aed8fb49
khsh90 <khsh400@gmail.com>
2022-05-21 15:01:03 +0300

    delivery app Max

---
+++ mealsappbymax/mealsappbymax/lib/category.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Category {
+  final String id;
+  final Color itemColor;
+  final String title;
+
+  Category({required this.id, required this.title, required this.itemColor});
+}
--- mealsappbymax/mealsappbymax/lib/main.dart
+++ mealsappbymax/mealsappbymax/lib/main.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return const MaterialApp(
+      home: DeliveryApp(),
+      title: 'Max DeliveryApp',
+    );
   }
 }
+
+
+
+class DeliveryApp extends StatefulWidget {
+  const DeliveryApp({Key? key}) : super(key: key);
+
+
+  @override
+  State<DeliveryApp> createState() => _DeliveryApp();
+}
+
+class _DeliveryApp extends State<DeliveryApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliveryApp'),
+      ),
+      body: const Center(
+        child: Text('DeliveryApp'),
+      ),
+    );
+  }
+}
+
