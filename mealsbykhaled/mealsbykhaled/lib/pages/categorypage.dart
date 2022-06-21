d253f1d77c0d9c1f290acec46a21386f34c972a5
khsh90 <khsh400@gmail.com>
2022-06-21 12:40:29 +0300

    meals category Page

---
+++ mealsbykhaled/mealsbykhaled/lib/pages/categorypage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Category Screen'),
+      ),
+      body: const Center(
+        child: Text('Category Screen'),
+      ),
+    );
+  }
+}
