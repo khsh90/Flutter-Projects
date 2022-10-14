613537c916916d25071efcf92ca36215f5eaacf1
khaled hadidi <khsh400@gmail.com>
2022-10-15 00:10:05 +0300

    start from flutter projects server

--- login/login/lib/core/res/applicationcons.dart
+++ login/login/lib/core/res/applicationcons.dart
@@ -2,8 +2,8 @@ import 'package:appwrite/appwrite.dart';
 
 class ApplicationConst {
   static const String projectId = '63391dff87fa8788023c';
-  // static const String apiEndpoint = 'http://192.168.1.102:2025/v1';
-  static const String apiEndpoint = 'http://46.185.128.40:2025/v1';
+  static const String apiEndpoint = 'http://192.168.1.102:2025/v1';
+  // static const String apiEndpoint = 'http://46.185.128.40:2025/v1';
   static Client client = Client();
   static const jobDatabaseId = '633a812040fe583bce0a';
   static const jobCollectionId = '633a8134751799d382e5';
